c506b5efade9b882ca40bfef7f5ecddc175a7ca2
mhmmd18 <mamad180200@gmail.com>
2024-05-21 09:34:37 +0700

    class

---
+++ dist/class.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('class', function () {
+    it('class', function () {
+        class Customer {
+        }
+        class Order {
+        }
+        const customer = new Customer();
+        const order = new Order();
+    });
+});
---
+++ test/class.test.ts
@@ -0,0 +1,13 @@
+describe('class', function() { 
+    it('class', function() { 
+        class Customer {
+
+        }
+        class Order {
+
+        }
+
+        const customer : Customer = new Customer();
+        const order = new Order();
+    });
+ })
