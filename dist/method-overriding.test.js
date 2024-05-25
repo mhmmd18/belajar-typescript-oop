725fea3010e250c99b25d0090422cec1a21110d9
mhmmd18 <mamad180200@gmail.com>
2024-05-25 23:33:03 +0700

    super method

--- dist/method-overriding.test.js
+++ dist/method-overriding.test.js
@@ -13,10 +13,20 @@ describe('method-overriding', function () {
             console.info(`Selamat datang ${name}, Nama saya ${this.name}, Saya adalah manager`);
         }
     }
+    class Department extends Employee {
+        sayHello(name) {
+            super.sayHello(name);
+            console.info("saya adalah deparment");
+        }
+    }
     it('should method', function () {
         const employee = new Employee("Rian");
         employee.sayHello("Dwi");
         const manager = new Manager("Dwi");
         manager.sayHello("Rian");
+    });
+    it('should super method', function () {
+        const department = new Department("Hayyi");
+        department.sayHello("Dwi");
     });
 });
